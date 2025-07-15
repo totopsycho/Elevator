9155233bb575999fb0b5cf69af51b3c65ed007bd
TryKTel <trittener@yahoo.fr>
2025-07-15 18:06:52 +0300

    1.4 Création du button base
    On crée notre première acteur c++ button base et on en fait un blueprint

---
+++ Source/Elevator/Private/Core/Gameplay/ButtonBase.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Core/Gameplay/ButtonBase.h"
+
+// Sets default values
+AButtonBase::AButtonBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AButtonBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AButtonBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
