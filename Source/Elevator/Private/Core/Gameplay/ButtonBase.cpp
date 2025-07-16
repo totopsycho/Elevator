0233a6cdca504ce18e29b050dc93a6c65e3e5a98
TryKTel <trittener@yahoo.fr>
2025-07-16 11:38:36 +0300

    1.5 StaticMeshComponent
    Ajout de notre première static mesh component

--- Source/Elevator/Private/Core/Gameplay/ButtonBase.cpp
+++ Source/Elevator/Private/Core/Gameplay/ButtonBase.cpp
@@ -9,6 +9,8 @@ AButtonBase::AButtonBase()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	CacheButtonMesh = CreateDefaultSubobject<UStaticMeshComponent>("Cache Button Mesh");
+
 }
 
 // Called when the game starts or when spawned
