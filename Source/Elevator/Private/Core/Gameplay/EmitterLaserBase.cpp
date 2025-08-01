8cb5d3d06a7965367c4d7c724cecaff1dd81691a
TryKTel <trittener@yahoo.fr>
2025-08-01 11:33:48 +0300

    4.1 Composants du laser

---
+++ Source/Elevator/Private/Core/Gameplay/EmitterLaserBase.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Core/Gameplay/EmitterLaserBase.h"
+
+// Sets default values
+AEmitterLaserBase::AEmitterLaserBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = SceneComp = CreateDefaultSubobject<USceneComponent>("Scene Comp");
+
+	EmitterMesh = CreateDefaultSubobject<UStaticMeshComponent>("Emitter Mesh");
+	EmitterMesh->SetupAttachment(RootComponent);
+
+	ViewCylinder = CreateDefaultSubobject<UStaticMeshComponent>("View Cylinder");
+	ViewCylinder->SetupAttachment(EmitterMesh);
+}
+
+// Called when the game starts or when spawned
+void AEmitterLaserBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AEmitterLaserBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/Elevator/Public/Core/Gameplay/EmitterLaserBase.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EmitterLaserBase.generated.h"
+
+UCLASS()
+class ELEVATOR_API AEmitterLaserBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AEmitterLaserBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY()
+	USceneComponent* SceneComp;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* EmitterMesh;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* ViewCylinder;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
