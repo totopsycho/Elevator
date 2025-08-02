6fb84579c6ed6e39e95d4ad7103044f8a42ee7f3
TryKTel <trittener@yahoo.fr>
2025-08-02 13:29:21 +0300

    4.6 Sphere Trace part 2

--- Source/Elevator/Elevator.h
+++ Source/Elevator/Elevator.h
@@ -3,3 +3,21 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
+//SpherTraceByChannel
+/*FVector Start = EmitterMesh->GetComponentLocation();
+FVector UpVectorMultiplied = EmitterMesh->GetUpVector() * TraceMaxDistance;
+FVector End = Start + UpVectorMultiplied;
+float Radius = EmitterScale * 50.f;
+TArray<AActor*> ActorsToIgnore;
+ActorsToIgnore.Add(this);
+FHitResult OutHit;
+
+bool bIsHit = UKismetSystemLibrary::SphereTraceSingle(GetWorld(), Start, End, Radius, UEngineTypes::ConvertToTraceType(ECC_Visibility), false,
+	ActorsToIgnore, EDrawDebugTrace::ForOneFrame, OutHit, true);
+
+if (bIsHit)
+{
+	AActor* HitActor = OutHit.GetActor();
+	UE_LOG(LogTemp, Warning, TEXT("Actor Name is %s"), *HitActor->GetName())
+}*/
--- Source/Elevator/Private/Core/Gameplay/EmitterLaserBase.cpp
+++ Source/Elevator/Private/Core/Gameplay/EmitterLaserBase.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Core/Gameplay/EmitterLaserBase.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 // Sets default values
 AEmitterLaserBase::AEmitterLaserBase()
@@ -39,11 +40,25 @@ void AEmitterLaserBase::BeginPlay()
 
 void AEmitterLaserBase::SphereTrace()
 {
-	UE_LOG(LogTemp, Log, TEXT("Timer"))
 	FVector Start = EmitterMesh->GetComponentLocation();
 	FVector UpVectorMultiplied = EmitterMesh->GetUpVector() * TraceMaxDistance;
 	FVector End = Start + UpVectorMultiplied;
 	float Radius = EmitterScale * 50.f;
+	TArray<AActor*> ActorsToIgnore;
+	ActorsToIgnore.Add(this);
+	FHitResult OutHit;
+	
+	bool bIsHit =UKismetSystemLibrary::SphereTraceSingle(GetWorld(), Start, End, Radius, UEngineTypes::ConvertToTraceType(ECC_Visibility), false,
+		ActorsToIgnore, EDrawDebugTrace::ForOneFrame,OutHit, true );
+
+	if (bIsHit)
+	{
+		AActor* HitActor = OutHit.GetActor();
+		UE_LOG(LogTemp, Warning, TEXT("Actor Name is %s"), *HitActor->GetName())
+	}
+
+
+
 }
 
 // Called every frame
